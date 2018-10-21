6596afc3356dddfed9d78814047b9a2d174658aa
陆新宗 <915212291@qq.com>
2018-10-21 21:54:18 +0800

    1.4

--- src/main/java/com/suma/controller/AdvCheckDetailController.java
+++ src/main/java/com/suma/controller/AdvCheckDetailController.java
@@ -7,13 +7,9 @@ import com.suma.service.AdvCheckService;
 import com.suma.service.AdvInfoService;
 import com.suma.utils.Result;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author luxinzong
